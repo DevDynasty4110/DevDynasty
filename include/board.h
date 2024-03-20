e03a0a0d1ca9f0d1dab98490ea997c5562e651c2
Liam Tolkkinen <liamtolkki@gmail.com>
2024-03-19 22:44:15 -0400

    used macros for the constants for easier use. Added Destructor

--- include/board.h
+++ include/board.h
@@ -1,13 +1,18 @@
 #include <string>
-
+#define __EASY 0
+#define __EASY_SCALAR 0.5
+#define __MEDIUM 1
+#define __MEDIUM_SCALAR 1.0
+#define __HARD 2
+#define __HARD_SCALAR 1.5
 class Board
 {
-    private:
-        int nTiles;
-        float scoreScalar;
+private:
+    int nTiles;
+    double scoreScalar;
 
-    public:
-        Board(int difficulty);
-
-
+public:
+    Board(); // default constructor
+    Board(int difficulty);
+    ~Board(); // default destructor
 };
--- src/board.cpp
+++ src/board.cpp
@@ -1,25 +1,27 @@
 #include "../include/board.h"
 
-/* 
+/*
  *   difficulty passed in as int from user input
  *   0 = easy, 1 = medium, 2 = hard
  *   probably add nested for loop in initializer to fill out board (matrix)
- */ 
+ */
 Board::Board(int difficulty)
 {
     nTiles = 0;
-    if (difficulty == 0)
-     {
-        scoreScalar = 0.5;
-     }
-    else if (difficulty == 1)
-    {  
-        scoreScalar = 1.0;
+    if (difficulty == __EASY)
+    {
+        scoreScalar = __EASY_SCALAR;
+    }
+    else if (difficulty == __MEDIUM)
+    {
+        scoreScalar = __MEDIUM_SCALAR;
+    }
+    else if (difficulty == __HARD_SCALAR)
+    {
+        scoreScalar = __HARD_SCALAR;
     }
     else
     {
-        scoreScalar = 1.5;
+        exit(-1); // invalid difficulty
     }
 }
-
-
