c377096594cf1c89836b830ecdba96a0ca6fe844
mpsloan <sloan.40@wright.edu>
2024-03-19 22:10:55 -0400

    add basic initializer code

--- src/board.cpp
+++ src/board.cpp
@@ -1,14 +1,25 @@
 #include "../include/board.h"
 
-Board::Board(int difficulty):
+/* 
+ *   difficulty passed in as int from user input
+ *   0 = easy, 1 = medium, 2 = hard
+ *   probably add nested for loop in initializer to fill out board (matrix)
+ */ 
+Board::Board(int difficulty)
 {
     nTiles = 0;
     if (difficulty == 0)
+     {
         scoreScalar = 0.5;
+     }
     else if (difficulty == 1)
+    {  
         scoreScalar = 1.0;
+    }
     else
+    {
         scoreScalar = 1.5;
+    }
 }
 
 
