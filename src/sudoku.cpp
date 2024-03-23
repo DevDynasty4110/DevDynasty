043a95a9b99f1121ae31e638128c25b6576ceb75
mpsloan <sloan.40@wright.edu>
2024-03-23 17:19:25 -0400

    add main function back

--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -1,6 +1,15 @@
 #include "../include/sudoku.h"
+#include <iostream>
+#include "../include/board.h"
 
 void Game::startGame()
 {
     // TODO
+}
+
+int main()
+{
+    Board board(0);
+    std::cout << board << std::endl;
+    return 0;
 }
