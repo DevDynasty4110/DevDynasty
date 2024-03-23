409150e79d0d4f43d67281b940b124662d237ad8
mpsloan <sloan.40@wright.edu>
2024-03-23 17:19:02 -0400

    add print header

--- include/board.h
+++ include/board.h
@@ -1,14 +1,17 @@
 #include <string>
+#include <iostream>
 #define __EASY 0
 #define __EASY_SCALAR 0.5
 #define __MEDIUM 1
 #define __MEDIUM_SCALAR 1.0
 #define __HARD 2
 #define __HARD_SCALAR 1.5
+#define __COLUMN 9
+#define __ROW 9
 class Board
 {
 private:
-    int *board[][];
+    int board[__ROW][__COLUMN];
     int nTiles;
     double scoreScalar;
 
@@ -16,4 +19,5 @@ public:
     Board(); // default constructor
     Board(int difficulty);
     ~Board(); // default destructor
+    friend std::ostream& operator<<(std::ostream &os, const Board& b);
 };
